e00498b6415bfe0a153c071c7022f9de32c54768
sevenblackthorns <161628029+sevenblackthorns@users.noreply.github.com>
2024-03-04 18:58:20 +0100

    Create test_train.js

---
+++ js/test_train.js
@@ -0,0 +1,5 @@
+import {SimpleArrayRank, ArrayShape, PrintArray} from "./js/arraymath.js";
+let Array0 = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]];
+console.log(SimpleArrayRank(Array0));
+PrintArray(ArrayShape(Array0));
+PrintArray(Array0);
