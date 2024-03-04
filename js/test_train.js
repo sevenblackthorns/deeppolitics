8c46788b8dad1ec6832893dae4f0fd52e6fb33e9
sevenblackthorns <161628029+sevenblackthorns@users.noreply.github.com>
2024-03-04 19:26:47 +0100

    Update test_train.js

--- js/test_train.js
+++ js/test_train.js
@@ -3,4 +3,3 @@ let Array0 = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]];
 console.log(SimpleArrayRank(Array0));
 PrintArray(ArrayShape(Array0));
 PrintArray(Array0);
-console.log("PLEASE END THIS.");
