223d6d682bcf9c6fd534b44fb029db431f930754
sevenblackthorns <161628029+sevenblackthorns@users.noreply.github.com>
2024-03-05 09:14:15 +0100

    Update arraymath.js

--- js/arraymath.js
+++ js/arraymath.js
@@ -44,9 +44,6 @@ export function ArrayShape(Array0) {
 }
 
 export function PrintArray(Array0, Log) {
-    if (Log === undefined) {
-        Log = true;
-    }
     if (Array.isArray(Array0)) {
         let Output = "[";
         let ArrLength = Array0.length;
