965f65a9621a0427b369c850e50a172953d52ad6
sevenblackthorns <161628029+sevenblackthorns@users.noreply.github.com>
2024-03-05 10:12:37 +0100

    Update arraymath.js

--- js/arraymath.js
+++ js/arraymath.js
@@ -29,7 +29,7 @@ export function ArrayShape(Array0) {
             let ArrEl0Shape = ArrayShape(Array0[0]);
             for (let ArrIndex = 1; ArrIndex < ArrLength; ArrIndex++) {
                 if (SimpleArrayComparison([ArrEl0Shape, ArrayShape(Array0[ArrIndex])])) {
-                    throw "DEEP:0 - INVALID SHAPE ERROR";
+                    throw "DEEP:0 - INVALID SHAPE ERROR.";
                 }
             }
             return [ArrLength].concat(ArrEl0Shape);
@@ -81,6 +81,9 @@ export function PrintArray(Array0) {
 }
 
 export function ArrayAdd(Array0, Array1) {
+    if (Array.isArray(Array0) == false and Array.isArray(Array1) == false) {
+        return Array0 + Array1;
+    }
     let Rank0 = SimpleArrayRank(Array0);
     let Rank1 = SimpleArrayRank(Array0);
     let DepthDiff = math.abs(Rank0 - Rank1);
@@ -95,6 +98,23 @@ export function ArrayAdd(Array0, Array1) {
         }
     }
     let Shape0 = ArrayShape(Array0);
+    if (Shape0[Shape0.length - 1] == 0) {
+        Shape0.pop();
+    }
+    if (Shape1[Shape1.length - 1] == 0) {
+        Shape1.pop();
+    }
     let Shape1 = ArrayShape(Array1);
-    
+    for (let i=0; i < Shape0.length; i++) {
+        if (Shape0[i] % Shape1[i] == 0 or Shape1[i] % Shape0[i] == 0) {
+            let Array2 = [];
+            for (let i=0; i < math.max(Shape0[0], Shape1[0]); i++) {
+                Array2[i] = ArrayAdd(Array0[i % Shape0[0]], Array1[i % Shape0[1]]);
+            }
+            return Array2;
+        }
+        else {
+            throw "DEEP:1 - INCOMPATIBLE SHAPES ERROR.";
+        }
+    }
 }
