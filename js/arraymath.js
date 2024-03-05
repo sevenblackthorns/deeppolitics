adf2c7f45abcbedb4cacd4f1b96a300d3db18683
sevenblackthorns <161628029+sevenblackthorns@users.noreply.github.com>
2024-03-05 09:28:40 +0100

    Update arraymath.js

--- js/arraymath.js
+++ js/arraymath.js
@@ -48,9 +48,9 @@ export function PrintArrayLogless(Array0) {
         let Output = "[";
         let ArrLength = Array0.length;
         if (ArrLength > 0) {
-            Output = Output.concat(PrintArray(Array0[0], Log=false));
+            Output = Output.concat(PrintArray(Array0[0]));
             for (let ArrIndex = 1; ArrIndex < ArrLength; ArrIndex++) {
-                Output = Output.concat(", ", PrintArray(Array0[ArrIndex], Log=false));
+                Output = Output.concat(", ", PrintArray(Array0[ArrIndex]));
             }
         }
         Output = Output.concat("]");
@@ -66,7 +66,7 @@ export function PrintArray(Array0) {
         let Output = "[";
         let ArrLength = Array0.length;
         if (ArrLength > 0) {
-            Output = Output.concat(PrintArrayLogless(Array0[0]);
+            Output = Output.concat(PrintArrayLogless(Array0[0]));
             for (let ArrIndex = 1; ArrIndex < ArrLength; ArrIndex++) {
                 Output = Output.concat(", ", PrintArrayLogless(Array0[ArrIndex]);
             }
