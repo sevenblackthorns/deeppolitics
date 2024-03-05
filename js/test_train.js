3fad7ad0b0cf17ed77730ef444a3c6f4db98c808
sevenblackthorns <161628029+sevenblackthorns@users.noreply.github.com>
2024-03-05 10:13:43 +0100

    Update test_train.js

--- js/test_train.js
+++ js/test_train.js
@@ -1,5 +1,7 @@
-import {SimpleArrayRank, ArrayShape, PrintArray} from "./arraymath.js";
+import {SimpleArrayRank, ArrayShape, ArrayAdd, PrintArray} from "./arraymath.js";
 let Array0 = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]];
 console.log(SimpleArrayRank(Array0));
 PrintArray(ArrayShape(Array0), true);
 PrintArray(Array0, true);
+Array1 = ArrayAdd(Array0, Array0);
+PrintArray(Array1);
