d81f4ad92c6295abac3c1b45097d23cbdc8c0688
sevenblackthorns <161628029+sevenblackthorns@users.noreply.github.com>
2024-03-07 10:11:26 +0100

    Update test_train.js

--- js/test_train.js
+++ js/test_train.js
@@ -1,5 +1,5 @@
 import {SimpleArrayRank, ArrayShape, ArrayOp, PrintArray, ArrayReshape, ArrayIndex, ArrayDot} from "./arraymath.js";
-let Array0 = [0, 1, 0, 1, 0, 1, 0]];
+let Array0 = [[0, 1, 0, 1, 0, 1, 0]];
 let Array1 = [[0, 1, 2], [0, 1, 2], [0, 0, 1], [2, 0, 1], [2, 0, 0], [1, 2, 0], [1, 2, 0]]
 PrintArray(ArrayDot(Array0, Array1, [0, 1]));
 
