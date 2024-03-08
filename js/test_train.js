77f9a8201cd440fad16d25b14f493a4c17c5d6f3
sevenblackthorns <161628029+sevenblackthorns@users.noreply.github.com>
2024-03-08 19:30:00 +0100

    Update test_train.js

--- js/test_train.js
+++ js/test_train.js
@@ -1,4 +1,4 @@
 import {SimpleArrayRank, ArrayShape, ArrayOp, PrintArray, ArrayReshape, ArrayIndex, ArrayDot, ArrayRandom, ArrayFill, ArrayTranspose, ArrayWhere} from "./arraymath.js";
 let Array0 = ArrayRandom([5, 5], -10, 10);
 PrintArray(Array0);
-PrintArray(ArrayWhere(ArrayOp(Array0, 0, ">")), Array0, ArrayFill([5, 5], 0));
+PrintArray(ArrayWhere(ArrayOp(Array0, 0, ">"), Array0, ArrayFill([5, 5], 0)));
