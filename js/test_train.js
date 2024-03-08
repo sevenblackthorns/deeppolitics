f66506f2cf0e12204490c2089785ad43e357e416
sevenblackthorns <161628029+sevenblackthorns@users.noreply.github.com>
2024-03-08 18:25:45 +0100

    Update test_train.js

--- js/test_train.js
+++ js/test_train.js
@@ -5,5 +5,8 @@ PrintArray(ArrayShape(Array0));
 let Array1 = ArrayRandom([100, 5, 8, 10], -1, 1);
 PrintArray(Array1);
 PrintArray(ArrayShape(Array1));
-PrintArray(ArrayDot(Array0, ArrayTranspose(Array1)));
-PrintArray(ArrayShape(ArrayDot(Array0, ArrayTranspose(Array1))));
+let Array1T = ArrayTranspose(Array1);
+PrintArray(ArrayShape(Array1T));
+let Array2 = ArrayDot(Array0, Array1T);
+PrintArray(Array2);
+PrintArray(ArrayShape(Array2))
