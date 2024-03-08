a248d405892e237ddde97392dc5543a0c2ac5be5
sevenblackthorns <161628029+sevenblackthorns@users.noreply.github.com>
2024-03-08 20:36:09 +0100

    Update test_train.js

--- js/test_train.js
+++ js/test_train.js
@@ -9,10 +9,14 @@ for (let Epoch=0; Epoch < 100; Epoch++) {
         for (let Layer=0; Layer < Layers.length; Layer++) {
             X = Layers[Layer].forward(X);
         }
+        console.log("X:");
         PrintArray(X);
+        console.log("Y:");
         PrintArray(Y);
+        console.log("MSE:");
         PrintArray(MSE(X, Y));
         let Gradient = MSEd(X, Y);
+        console.log("GRADIENT:");
         PrintArray(Gradient);
         for (let Layer=Layers.length - 1; Layer >= 0; Layer--) {
             Gradient = Layers[Layer].backward(Gradient, 0.01);
