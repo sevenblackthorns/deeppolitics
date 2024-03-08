6d8d348efbd4b69c9584d408c8db82673b418ccc
sevenblackthorns <161628029+sevenblackthorns@users.noreply.github.com>
2024-03-08 20:23:45 +0100

    Update test_train.js

--- js/test_train.js
+++ js/test_train.js
@@ -1,4 +1,19 @@
-import {SimpleArrayRank, ArrayShape, ArrayOp, PrintArray, ArrayReshape, ArrayIndex, ArrayDot, ArrayRandom, ArrayFill, ArrayTranspose, ArrayWhere} from "./arraymath.js";
-let Array0 = ArrayRandom([5, 5], -10, 10);
-PrintArray(Array0);
-PrintArray(ArrayWhere(ArrayOp(Array0, 0, ">"), Array0, ArrayFill([5, 5], 0)));
+import {SimpleArrayRank, ArrayShape, ArrayOp, PrintArray, ArrayReshape, ArrayIndex, ArrayDot, ArrayRandom, ArrayFill, ArrayTranspose, ArrayWhere} from "arraymath.js";
+import {Dense, PReLU, MSE, MSEd} from "neuralnetwork.js"
+let Layers = [Dense(4, 2), PReLU(0.01), Dense(2, 2), PReLU(0.01)];
+let Data = [[[0, 1, 0, 1], [0, 1]], [[0, 1, 1, 0], [1, 0]], [[1, 0, 0, 1], [1, 0]], [[1, 0, 1, 0], [1, 0]]];
+for (let Epoch=0; Epoch < 100; Epoch++) {
+    for (let i=0; i < Data.length; i++) {
+        let X = ArrayReshape(Data[i][0], [1, 4]);
+        let Y = ArrayReshape(Data[i][1], [1, 2]);
+        for (let Layer=0; Layer < Layers.length; Layer++) {
+            X = Layers[Layer].forward(X);
+        }
+        ArrayPrint(MSE(X, Y));
+        let Gradient = MSEd(X, Y);
+        ArrayPrint(Gradient);
+        for (let Layer=Layers.length - 1; Layer >= 0; Layer--) {
+            Gradient = Layers[Layer].backward(Gradient);
+        }
+    }
+}
