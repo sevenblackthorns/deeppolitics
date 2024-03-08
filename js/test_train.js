28736718dc750f2d5718e08ba329e45ac2821ae0
sevenblackthorns <161628029+sevenblackthorns@users.noreply.github.com>
2024-03-08 18:01:47 +0100

    Update test_train.js

--- js/test_train.js
+++ js/test_train.js
@@ -1,9 +1,9 @@
-import {SimpleArrayRank, ArrayShape, ArrayOp, PrintArray, ArrayReshape, ArrayIndex, ArrayDot, ArrayRandom} from "./arraymath.js";
+import {SimpleArrayRank, ArrayShape, ArrayOp, PrintArray, ArrayReshape, ArrayIndex, ArrayDot, ArrayRandom, ArrayTranspose} from "./arraymath.js";
 let Array0 = ArrayRandom([100, 5, 8, 10], -1, 1);
 PrintArray(Array0);
 PrintArray(ArrayShape(Array0));
 let Array1 = ArrayRandom([100, 5, 8, 10], -1, 1);
 PrintArray(Array1);
 PrintArray(ArrayShape(Array1));
-PrintArray(ArrayDot(Array0, Array1, [1, 4]));
-PrintArray(ArrayShape(ArrayDot(Array0, Array1, [1, 4])));
+PrintArray(ArrayDot(Array0, ArrayTranspose(Array1, [0, 1]), [0, 1]));
+PrintArray(ArrayShape(ArrayDot(Array0, ArrayTranspose(Array1), [0, 1])));
