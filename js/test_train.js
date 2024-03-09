17ca5358328fbedef51ed56c6c26e738abcb4334
sevenblackthorns <161628029+sevenblackthorns@users.noreply.github.com>
2024-03-09 11:49:25 +0100

    Update test_train.js

--- js/test_train.js
+++ js/test_train.js
@@ -3,7 +3,7 @@ import {Dense, PReLU, MSE, MSEd, Softmax} from "./neuralnetwork.js";
 
 let Layers = [new Dense(4, 2), new PReLU(0.01), new Dense(2, 2), new PReLU(0.01), new Softmax()];
 let Data = [[[0, 1, 0, 1], [0, 1]], [[0, 1, 1, 0], [1, 0]], [[1, 0, 0, 1], [1, 0]], [[1, 0, 1, 0], [1, 0]]];
-for (let Epoch=0; Epoch < 400; Epoch++) {
+for (let Epoch=0; Epoch < 1000; Epoch++) {
     let Loss = 0;
     for (let i=0; i < Data.length; i++) {
         let X = ArrayReshape(Data[i][0], [1, 4]);
@@ -23,7 +23,7 @@ for (let Epoch=0; Epoch < 400; Epoch++) {
         console.log("GRADIENT:");
         PrintArray(Gradient);
         for (let Layer=Layers.length - 1; Layer >= 0; Layer--) {
-            Gradient = Layers[Layer].backward(Gradient, 0.05);
+            Gradient = Layers[Layer].backward(Gradient, 0.1);
         }
     }
     console.log(Loss / Data.length);
