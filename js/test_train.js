6211cfd1aeb4118304041a6ba8cd1b8e3329f7f3
sevenblackthorns <161628029+sevenblackthorns@users.noreply.github.com>
2024-03-09 11:27:14 +0100

    Update test_train.js

--- js/test_train.js
+++ js/test_train.js
@@ -1,7 +1,7 @@
 import {SimpleArrayRank, ArrayShape, ArrayOp, PrintArray, ArrayReshape, ArrayIndex, ArrayDot, ArrayRandom, ArrayFill, ArrayTranspose, ArrayWhere} from "./arraymath.js";
 import {Dense, PReLU, MSE, MSEd, Softmax} from "./neuralnetwork.js";
-/*
-let Layers = [new Dense(4, 2), new PReLU(0.01), new Dense(2, 2), new PReLU(0.01)];
+
+let Layers = [new Dense(4, 2), new PReLU(0.01), new Dense(2, 2), new PReLU(0.01), new Softmax()];
 let Data = [[[0, 1, 0, 1], [0, 1]], [[0, 1, 1, 0], [1, 0]], [[1, 0, 0, 1], [1, 0]], [[1, 0, 1, 0], [1, 0]]];
 for (let Epoch=0; Epoch < 400; Epoch++) {
     let Loss = 0;
@@ -24,38 +24,6 @@ for (let Epoch=0; Epoch < 400; Epoch++) {
         PrintArray(Gradient);
         for (let Layer=Layers.length - 1; Layer >= 0; Layer--) {
             Gradient = Layers[Layer].backward(Gradient, 0.05);
-        }
-    }
-    console.log(Loss / Data.length);
-}
-*/
-
-let Layers = [new Dense(100, 50), new PReLU(0.01), new Dense(50, 10), new PReLU(0.01), new Softmax()];
-let Data = [];
-for (let i=0; i < 100; i++) {
-    Data.push([ArrayRandom([1, 100], -5, 5), ArrayRandom([1, 10], -10, 10)])
-}
-for (let Epoch=0; Epoch < 10; Epoch++) {
-    let Loss = 0;
-    for (let i=0; i < Data.length; i++) {
-        let X = Data[i][0];
-        let Y = Data[i][1];
-        for (let Layer=0; Layer < Layers.length; Layer++) {
-            X = Layers[Layer].forward(X);
-        }
-        console.log("X:");
-        PrintArray(X);
-        console.log("Y:");
-        PrintArray(Y);
-        console.log("MSE:");
-        let Err = MSE(X, Y);
-        console.log(Err);
-        Loss += Err;
-        let Gradient = MSEd(X, Y);
-        console.log("GRADIENT:");
-        PrintArray(Gradient);
-        for (let Layer=Layers.length - 1; Layer >= 0; Layer--) {
-            Gradient = Layers[Layer].backward(Gradient, 0.1);
         }
     }
     console.log(Loss / Data.length);
